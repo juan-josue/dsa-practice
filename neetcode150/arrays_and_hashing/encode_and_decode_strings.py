9583c98a5d522d4bfe9886455d9d44309d47cec7
juan-josue <juansemail2001@gmail.com>
2025-06-08 23:56:02 -0400

    add comments to encode decode strings solution

--- neetcode150/arrays_and_hashing/encode_and_decode_strings.py
+++ neetcode150/arrays_and_hashing/encode_and_decode_strings.py
@@ -3,6 +3,8 @@ from typing import List
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
+        # Encoded strings are the len of the str following by #
+        # Example: ["hello", "world"] -> "5#hello5#world"
         res = ''
         for s in strs:
             s_encoded = str(len(s)) + '#' + s
@@ -10,9 +12,11 @@ class Solution:
         return res
 
     def decode(self, s: str) -> List[str]:
+        # Decode the string by finding the length of each str
+        # Example: ["hello", "world"] <- "5#hello5#world"
         res = []
         i = 0
-
+        
         while i < len(s):
             j = s.index('#', i)
             len_str = int(s[i : j])
